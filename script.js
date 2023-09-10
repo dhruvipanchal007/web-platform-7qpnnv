a731e1b753cfab4470d4db2bdcf0accfb1a4250b
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-10 12:06:26 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log('Hello!');
+
+const display = document.querySelector('#inputVal');
+const buttons = document.querySelectorAll('.calc-btn');
+let userInput = '';
+
+const doCalculation = () => {
+  /* const firstNum = +userInput[0];
+const operator = userInput[1];
+const secondNum = +userInput[2]; */
+  const [firstNum, operator, secondNum] = userInput;
+
+  let ans = '';
+  switch (operator) {
+    case '+':
+      ans = firstNum + secondNum;
+      break;
+    case '*':
+      ans = firstNum * secondNum;
+      break;
+  }
+  return ans;
+};
+
+buttons.forEach((currBtn) => {
+  currBtn.addEventListener('click', (event) => {
+    userInput = userInput + event.target.innerText;
+    display.value = userInput;
+
+    if (event.target.innerText === '=') {
+      const finalAns = doCalculation();
+      display.value = finalAns;
+    }
+  });
+});

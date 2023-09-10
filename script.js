b334f35705e0c2f0c07a552b133d94d104ee6dd1
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-10 13:06:10 +0530

    1 file modified

--- script.js
+++ script.js
@@ -5,37 +5,42 @@ const buttons = document.querySelectorAll('.calc-btn');
 let userInput = '';
 
 const doCalculation = () => {
-  /* const firstNum = +userInput[0];
-const operator = userInput[1];
-const secondNum = +userInput[2]; */
-  const [firstNum, operator, secondNum] = userInput;
-
-  let ans = '';
-  switch (operator) {
-    case '+':
-      ans = firstNum + secondNum;
-      break;
-    case '*':
-      ans = firstNum * secondNum;
-      break;
-      case '/':
-      ans = firstNum / secondnum;
-      break;
-      case '-':
-      ans = firstNum-secondNum;
-      break;
-  }
+  // let [firstNum, operator, secondNum] = userInput;
+  const ans = eval(userInput);
   return ans;
+  //    const firstNum = +userInput[0];
+  // const operator = userInput[1];
+  // const secondNum = +userInput[2];
+
+  // let ans = '';
+  // switch (operator) {
+  //   case '+':
+  //     ans = +firstNum + +secondNum;
+  //     break;
+  //   case '*':
+  //     ans = +firstNum * +secondNum;
+  //     break;
+  //   case '/':
+  //     ans = +firstNum / +secondNum;
+  //     break;
+  //   case '-':
+  //     ans = +firstNum - +secondNum;
+  //     break;
+  // }
+  // return ans;
 };
 
 buttons.forEach((currBtn) => {
   currBtn.addEventListener('click', (event) => {
-    userInput = userInput + event.target.innerText;
-    display.value = userInput;
-
     if (event.target.innerText === '=') {
       const finalAns = doCalculation();
       display.value = finalAns;
+      userInput = finalAns;
+      // let xyz = finalAns;
+      // firstNum = xyz;
+    } else {
+      userInput = userInput + event.target.innerText;
+      display.value = userInput;
     }
   });
 });
